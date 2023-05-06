a807ef7a2a85b5abba43200925e898acb2754d86
Prafull Epili <prafullepili@gmail.com>
2023-05-06 11:31:26 +0530

    separated routes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const productsRoutes = require('./routes/products');
 
 
 app.get('/', (req, res) => {
     res.send("Hello Product API")
 })
+app.use('/api/v1/products', productsRoutes)
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`server listing port ${PORT}`)
---
+++ routes/products.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/').get((req, res) => {
+    return res.send("Products");
+})
+
+module.exports = router
