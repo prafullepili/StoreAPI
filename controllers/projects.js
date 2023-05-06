3f3c4a177db14b9f5b70031419575acbbd45b3ef
Prafull Epili <prafullepili@gmail.com>
2023-05-07 00:04:39 +0530

    select field by user feature

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,7 +1,7 @@
 const ProductModel = require('../models/products')
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, select } = req.query;
     const queryObj = {}
 
     if (featured) {
@@ -19,8 +19,14 @@ const getAllProducts = async (req, res) => {
     } else {
         sortBy = sort.split(',').join(' ');
     }
+
+    let fieldList
+    if (select) {
+        fieldList = select.split(',').join(' ')
+    }
+
     try {
-        const products = await ProductModel.find(queryObj).sort(sortBy);
+        const products = await ProductModel.find(queryObj).sort(sortBy).select(fieldList);
         res.status(200).json({ nbHits: products.length, products })
     } catch (error) {
         res.status(402).json({ error })
@@ -30,3 +36,15 @@ const getAllProducts = async (req, res) => {
 module.exports = {
     getAllProducts
 }
+
+
+
+
+// const input = "'price',name";
+// const output = input
+//     .split(",") // split the string into an array
+//     .reduce((acc, curr) => {
+//         acc[curr.replace(/'/g, '')] = 1; // remove the single quotes and add the key to the object
+//         return acc;
+//     }, {});
+// console.log(output); // { price: 1, name: 1 }
